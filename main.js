b1226c7ed2a96e943b429a3450d478b57a92eb18
Melina Kamyab <melina.kamyab@gmail.com>
2021-01-26 18:40:08 +0100

    create a zero-dependency module for storing secrets

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const router = require("./routes/todoRoute");
 const nodeSass = require("node-sass-middleware");
 const path = require("path");
+require("dotenv").config();
 
 const app = express();
 app.use(express.static(__dirname + "/public"));
@@ -19,13 +20,10 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.set("view engine", "ejs");
 app.use("/", router);
 
-mongoose.connect(
-  "mongodb+srv://Fed20s:test@cluster0.lfdxf.mongodb.net/ToDoList?retryWrites=true&w=majority",
-  (err) => {
-    console.log(err);
-    if (err) return;
-    app.listen(8000, () => {
-      console.log("app is running");
-    });
-  }
-);
+mongoose.connect(process.env.DATABASE_URL, (err) => {
+  console.log(err);
+  if (err) return;
+  app.listen(8000, () => {
+    console.log("app is running");
+  });
+});
