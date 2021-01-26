ea160fd1a356c05b2f41b9ef14faa5fddb5bc45c
Melina Kamyab <melina.kamyab@gmail.com>
2021-01-26 18:12:01 +0100

    add a route folder and import content to main.js

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const nodeSass = require("node-sass-middleware");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const router = require("./routes/todoRoute");
+const nodeSass = require("node-sass-middleware");
+const path = require("path");
 
 const app = express();
+app.use(express.static(__dirname + "/public"));
 
-app.use("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended: false}));
 app.use(
   nodeSass({
-    src: path.join(_dirname, "scss"),
-    dest: path.join(_dirname, "public"),
+    src: path.join(__dirname, "scss"),
+    dest: path.join(__dirname, "public"),
   })
+);
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.set("view engine", "ejs");
+app.use("/", router);
+
+mongoose.connect(
+  "mongodb+srv://Fed20s:test@cluster0.lfdxf.mongodb.net/ToDoList?retryWrites=true&w=majority",
+  (err) => {
+    console.log(err);
+    if (err) return;
+    app.listen(8000, () => {
+      console.log("app is running");
+    });
+  }
 );
