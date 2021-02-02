7c441b5e6a522a3eb7ea066290fb88e459d3019e
Melina Kamyab <melina.kamyab@gmail.com>
2021-02-02 16:30:37 +0100

    create route for CRUD

--- main.js
+++ main.js
@@ -8,8 +8,6 @@ const nodeSass = require("node-sass-middleware");
 const path = require("path");
 require("dotenv").config();
 
-const Todo = require("./model/todo");
-
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -22,41 +20,9 @@ app.use(
   })
 );
 
+app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 app.use("/", router);
-
-app.get("/", async (req, res) => {
-  const data = await Todo.find();
-  res.render("index.ejs", {data});
-});
-
-app.post("/", async (req, res) => {
-  await new Todo({
-    name: req.body.itemName,
-  }).save();
-  res.redirect("/");
-});
-
-app.get("/edit/:id", async (req, res) => {
-  const allData = await Todo.find();
-  const editOne = await Todo.findOne({_id: req.params.id});
-
-  res.render("edit.ejs", {
-    allData,
-    editOne,
-    error: "",
-  });
-});
-
-app.post("/edit", async (req, res) => {
-  await Todo.updateOne({_id: req.body.id}, {name: req.body.name});
-  res.redirect("/");
-});
-
-app.get("/delete/:id", async (req, res) => {
-  await Todo.deleteOne({_id: req.body.id});
-  res.redirect("/");
-});
 
 mongoose.connect(
   process.env.DATABASE_URL,
--- routes/todoRoute.js
+++ routes/todoRoute.js
@@ -2,6 +2,45 @@ const express = require("express");
 const Todo = require("../model/todo");
 const router = express.Router();
 
+router.get("/", async (req, res) => {
+  try {
+    const data = await Todo.find();
+    res.render("index.ejs", {data});
+  } catch (err) {
+    res.render("error.ejs", {error: err});
+  }
+});
 
+router.post("/", async (req, res) => {
+  try {
+    await new Todo({
+      name: req.body.itemName,
+    }).save();
+    res.redirect("/");
+  } catch (err) {
+    res.render("error.ejs", {error: err});
+  }
+});
+
+router.get("/delete/:id", async (req, res) => {
+  await Todo.deleteOne({_id: req.body.id});
+  res.redirect("/");
+});
+
+router.get("/edit/:id", async (req, res) => {
+  const allData = await Todo.find();
+  const editOne = await Todo.findOne({_id: req.params.id});
+
+  res.render("edit.ejs", {
+    allData,
+    editOne,
+    error: "",
+  });
+});
+
+router.post("/edit", async (req, res) => {
+  await Todo.updateOne({_id: req.body.id}, {name: req.body.name});
+  res.redirect("/");
+});
 
 module.exports = router;
